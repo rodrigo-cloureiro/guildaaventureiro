a8700809d8bebcf550669c26cf5221d158bb8407
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-02-12 16:56:05 -0300

    adicionando validação de companheiro; criando aventureiro com id automatico e ativo true por padrao; corrigindo validacao de nome e classe

--- src/main/java/br/com/infnet/guildaaventureiro/model/Aventureiro.java
+++ src/main/java/br/com/infnet/guildaaventureiro/model/Aventureiro.java
@@ -1,26 +1,35 @@
 package br.com.infnet.guildaaventureiro.model;
 
 import br.com.infnet.guildaaventureiro.enums.Classe;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import java.util.UUID;
 
 @Getter
 @ToString
-@RequiredArgsConstructor
 public class Aventureiro {
     private UUID id;
-    @NotNull(message = "O nome não pode ser vazio")
+    @NotBlank(message = "O nome não pode ser vazio")
     private String nome;
-    @NotNull
+    @NotNull(message = "A classe é obrigatória")
     private Classe classe;
     @Min(value = 1, message = "O nível deve ser maior ou igual a 1")
     private int nivel;
     @NotNull
     private Boolean ativo;
+    @Valid
     private Companheiro companheiro;
+
+    public Aventureiro(String nome, Classe classe, int nivel) {
+        this.id = UUID.randomUUID();
+        this.nome = nome;
+        this.classe = classe;
+        this.nivel = nivel;
+        this.ativo = true;
+    }
 }
