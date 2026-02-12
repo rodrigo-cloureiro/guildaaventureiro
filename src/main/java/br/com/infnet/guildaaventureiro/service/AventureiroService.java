59dedf77a1e4296d7be4b87cc3c5b480b60324f8
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-02-12 17:25:26 -0300

    método para criação do aventureiro no service

--- src/main/java/br/com/infnet/guildaaventureiro/service/AventureiroService.java
+++ src/main/java/br/com/infnet/guildaaventureiro/service/AventureiroService.java
@@ -1,7 +1,16 @@
 package br.com.infnet.guildaaventureiro.service;
 
+import br.com.infnet.guildaaventureiro.model.Aventureiro;
+import br.com.infnet.guildaaventureiro.repository.AventureiroRepositoryFake;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AventureiroService {
+    private final AventureiroRepositoryFake aventureiroRepository;
+
+    public Aventureiro criar(Aventureiro aventureiro) {
+        return aventureiroRepository.save(aventureiro);
+    }
 }
