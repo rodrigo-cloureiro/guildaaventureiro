c437122450645cd3da75acc05e1fb5bffade6e7c
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-02-12 19:41:02 -0300

    inicializando 100 registros

--- src/main/java/br/com/infnet/guildaaventureiro/repository/AventureiroRepositoryFake.java
+++ src/main/java/br/com/infnet/guildaaventureiro/repository/AventureiroRepositoryFake.java
@@ -1,17 +1,48 @@
 package br.com.infnet.guildaaventureiro.repository;
 
+import br.com.infnet.guildaaventureiro.enums.Classe;
 import br.com.infnet.guildaaventureiro.model.Aventureiro;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 @Repository
 public class AventureiroRepositoryFake {
     private final List<Aventureiro> banco = new ArrayList<>();
 
+    public AventureiroRepositoryFake() {
+        inicializar();
+    }
+
     public Aventureiro save(Aventureiro aventureiro) {
         banco.add(aventureiro);
         return aventureiro;
+    }
+
+    private void inicializar() {
+        Random random = new Random();
+
+        List<String> nomesPrefixos = Arrays.asList("Ragnar", "Elara", "Thrain", "Lyra",
+                "Kael", "Cedric", "Morganna", "Valerius");
+        List<String> nomesSufixos = Arrays.asList("o Bravo", "de Prata", "das Sombras",
+                "o Sábio", "de Ferro", "Luz do Sol");
+        List<String> classes = Arrays.stream(Classe.values()).map(Enum::name).toList();
+
+        for (int i = 0; i < 100; i++) {
+            String nome = nomesPrefixos.get(random.nextInt(nomesPrefixos.size()));
+            String sufixo = nomesSufixos.get(random.nextInt(nomesSufixos.size()));
+            String classe = classes.get(random.nextInt(classes.size()));
+            int nivel = random.nextInt(100) + 1;
+
+            Aventureiro aventureiro = new Aventureiro(
+                    String.format("%s %s", nome, sufixo),
+                    Classe.valueOf(classe),
+                    nivel
+            );
+            banco.add(aventureiro);
+        }
     }
 }
