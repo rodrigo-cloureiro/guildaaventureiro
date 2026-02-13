a6f66b4c6b90b9a5a0588155d563643e4309bd1b
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-02-13 17:13:44 -0300

    removendo responsabilidade de ordenacao do service

--- src/main/java/br/com/infnet/guildaaventureiro/service/AventureiroService.java
+++ src/main/java/br/com/infnet/guildaaventureiro/service/AventureiroService.java
@@ -18,45 +18,18 @@ import java.util.List;
 public class AventureiroService {
     private final AventureiroRepositoryFake aventureiroRepository;
 
-    /*
-        AINDA FALTA:
-           - Passar os filtros para o Repository:
-            - public List<Aventureiro> buscarComFiltro(AventureiroFiltroRequest filtro)
-            - aventureiroRepository.buscarComFiltro()
-    */
     public PagedResponse<Aventureiro> listar(
             @Valid AventureiroFiltroRequest filtro,
             int page,
             int size
     ) {
-        List<Aventureiro> filtrados = filtrar(filtro);
-        ordenar(filtrados, Comparator.comparing(Aventureiro::getId));
+        List<Aventureiro> filtrados = new ArrayList<>(
+                aventureiroRepository.findWithFilter(filtro, Comparator.comparing(Aventureiro::getId))
+        );
         return Paginator.paginate(filtrados, page, size);
     }
 
     public Aventureiro criar(Aventureiro aventureiro) {
         return aventureiroRepository.save(aventureiro);
-    }
-
-    private List<Aventureiro> filtrar(AventureiroFiltroRequest filtro) {
-        return new ArrayList<>(aventureiroRepository.findAll()
-                .stream()
-                .filter(a -> possuiFiltro(a.getClasse(), filtro.classe()))
-                .filter(a -> possuiFiltro(a.getAtivo(), filtro.ativo()))
-                .filter(a -> possuiFiltroNivelMinimo(a, filtro))
-                .toList()
-        );
-    }
-
-    private <T> boolean possuiFiltro(T objeto, T filtro) {
-        return filtro == null || objeto.equals(filtro);
-    }
-
-    private boolean possuiFiltroNivelMinimo(Aventureiro aventureiro, AventureiroFiltroRequest filtro) {
-        return filtro.nivelMinimo() == null || aventureiro.getNivel() >= filtro.nivelMinimo();
-    }
-
-    private void ordenar(List<Aventureiro> lista, Comparator<Aventureiro> comparator) {
-        lista.sort(comparator);
     }
 }
