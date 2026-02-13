f6b17a8471932a552b7211388c700c16f6ea1700
rodrigo.loureiro <rodrigo.loureiro@sondotecnica.com.br>
2026-02-13 16:39:50 -0300

    removendo logica de paginacao do service

--- src/main/java/br/com/infnet/guildaaventureiro/service/AventureiroService.java
+++ src/main/java/br/com/infnet/guildaaventureiro/service/AventureiroService.java
@@ -4,6 +4,7 @@ import br.com.infnet.guildaaventureiro.dto.AventureiroFiltroRequest;
 import br.com.infnet.guildaaventureiro.dto.PagedResponse;
 import br.com.infnet.guildaaventureiro.model.Aventureiro;
 import br.com.infnet.guildaaventureiro.repository.AventureiroRepositoryFake;
+import br.com.infnet.guildaaventureiro.util.Paginator;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -30,7 +31,7 @@ public class AventureiroService {
     ) {
         List<Aventureiro> filtrados = filtrar(filtro);
         ordenar(filtrados, Comparator.comparing(Aventureiro::getId));
-        return paginar(filtrados, page, size);
+        return Paginator.paginate(filtrados, page, size);
     }
 
     public Aventureiro criar(Aventureiro aventureiro) {
@@ -40,52 +41,22 @@ public class AventureiroService {
     private List<Aventureiro> filtrar(AventureiroFiltroRequest filtro) {
         return new ArrayList<>(aventureiroRepository.findAll()
                 .stream()
-                .filter(a -> filtroPossuiClasse(a, filtro))
-                .filter(a -> filtroPossuiAtivo(a, filtro))
-                .filter(a -> filtroPossuiNivelMinimo(a, filtro))
+                .filter(a -> possuiFiltro(a.getClasse(), filtro.classe()))
+                .filter(a -> possuiFiltro(a.getAtivo(), filtro.ativo()))
+                .filter(a -> possuiFiltroNivelMinimo(a, filtro))
                 .toList()
         );
     }
 
-    private boolean filtroPossuiClasse(Aventureiro aventureiro, AventureiroFiltroRequest filtro) {
-        return filtro.classe() == null || aventureiro.getClasse().equals(filtro.classe());
+    private <T> boolean possuiFiltro(T objeto, T filtro) {
+        return filtro == null || objeto.equals(filtro);
     }
 
-    private boolean filtroPossuiAtivo(Aventureiro aventureiro, AventureiroFiltroRequest filtro) {
-        return filtro.ativo() == null || aventureiro.getAtivo().equals(filtro.ativo());
-    }
-
-    private boolean filtroPossuiNivelMinimo(Aventureiro aventureiro, AventureiroFiltroRequest filtro) {
+    private boolean possuiFiltroNivelMinimo(Aventureiro aventureiro, AventureiroFiltroRequest filtro) {
         return filtro.nivelMinimo() == null || aventureiro.getNivel() >= filtro.nivelMinimo();
     }
 
     private void ordenar(List<Aventureiro> lista, Comparator<Aventureiro> comparator) {
         lista.sort(comparator);
-    }
-
-    private PagedResponse<Aventureiro> paginar(
-            List<Aventureiro> lista,
-            int page,
-            int size
-    ) {
-        int total = lista.size();
-
-        int fromIndex = calcularInicioDaLista(page, size, total);
-        int toIndex = calcularFinalDaLista(fromIndex, size, total);
-
-        return new PagedResponse<>(
-                page,
-                size,
-                total,
-                lista.subList(fromIndex, toIndex)
-        );
-    }
-
-    private int calcularInicioDaLista(int page, int size, int total) {
-        return Math.min(page * size, total);
-    }
-
-    private int calcularFinalDaLista(int fromIndex, int sizeNumber, int total) {
-        return Math.min((fromIndex + sizeNumber), total);
     }
 }
